25d66cea7e41961aea19e48d7d68b169d58153f4
frapator <mpigois@yahoo.fr>
2017-07-29 02:31:08 +0200

    ajout couleur de theme

--- Echecs/Theme.js
+++ Echecs/Theme.js
@@ -1,12 +1,12 @@
 class Theme {
-	
+
 	constructor() {
-		this.mColors = ["#4488AA", "#444444", "#AAAAAA"];
+		this.mColors = ["#4488AA", "#444444", "#AAAAAA", "#CC99FF"];
 		this.mIndex = 0;
-		
+
 		this.ThemeButton;
 	}
-	
+
 
 	CreateButton() {
 		this.ThemeButton = game.add.text(GameWidth - 120, 400, 'Thème', { font: '24px Arial', fill: '#fff' });
@@ -15,13 +15,13 @@ class Theme {
 		   myController.SwitchTheme();
 	   });
 	}
-	
+
 	Switch() {
 	   this.mIndex ++;
 	   if (this.mIndex >= this.mColors.length) this.mIndex = 0;
 	   this.Select(this.mIndex);
 	}
-	 
+
 	Select(pIndex) {
 		this.ThemeButton.text = "Thème #" + (pIndex + 1);
 		game.stage.backgroundColor = this.mColors[pIndex];
